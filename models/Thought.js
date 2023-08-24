baf99ea47c7b69753f2e546aa5ea08e1097e6f45
Joel <joellongaresjr@gmail.com>
2023-08-24 01:55:25 -0700

    installed moment.js to formulate dates, finished Thought Schema

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require("mongoose");
+
+const moment = require('moment')
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] h:mm a")
+        }
+    },
+    {
+        toJSON: {virtuals: true, getters: true}
+    }
+
+
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: DataTransfer,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] h:mm a") 
+            // used moment.js as new built in package to format dates instead of using an utils folder
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: { virtuals: true, getters: true}
+    }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = { Thought };
